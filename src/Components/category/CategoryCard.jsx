1f98db1b447ba22ac5612fc0d418496f370f9c07
haymanot <haymanotebabu2@gmail.com>
2026-01-30 17:03:06 +0300

    the product card commit

--- src/Components/category/CategoryCard.jsx
+++ src/Components/category/CategoryCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CategoryCard.css";
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 function CategoryCard(data ) {
   // {data} ብለው ያስተካክሉት
@@ -9,14 +9,14 @@ function CategoryCard(data ) {
     <div className="category-card-container">
       
       {/* Class ጨምረናል */}
-      <Link to={`/category/${data.title}`} className="category-card-link">
+      <a href={`/category/${data.title}`} className="category-card-link">
 
         <span>
           <h2>{data.title}</h2>
         </span>
         <img src={data.imageLink} alt={data.title} />
         <p>Shop now</p>
-      </Link>
+      </a>
     </div>
   );
 }
