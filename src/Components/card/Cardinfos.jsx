c410a93297516c6f4e13af150fa50015a229bbd9
haymanot <haymanotebabu2@gmail.com>
2026-01-30 17:30:28 +0300

    the cardin fo jsx commit

---
+++ src/Components/card/Cardinfos.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './CardInfos.css'
+import {CardinformationData} from './CardinformationData.js'
+import {Link} from 'react-router-dom'
+
+function CardInfos ({data}) {
+   // console.log(data.imageLink);
+  return (
+    <div className="card-information">
+      <a href=''>
+        <h1>{data.id}</h1>
+        <span className="card-img">
+          <h1>{data.title}</h1>
+        </span>
+        <img src={data.imageLink} alt={data.title} />
+        <p className="img-description">{data.description}</p>
+        <p className="shopping know">shopping Know</p>
+      </a>
+      
+    </div>
+  );
+}
+
+export default CardInfos;
