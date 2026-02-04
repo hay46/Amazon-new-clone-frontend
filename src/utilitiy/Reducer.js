3a1297649f1501f420bf74d94bdfd78f015b74a1
haymanot <haymanotebabu2@gmail.com>
2026-02-05 00:00:52 +0300

    the derducer page is commit

--- src/utilitiy/Reducer.js
+++ src/utilitiy/Reducer.js
@@ -1,4 +1,3 @@
-import { UNSAFE_RSCDefaultRootErrorBoundary } from "react-router-dom";
 import { type } from "./Action";
 
 export const initialState = {
@@ -7,24 +6,24 @@ export const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case type.ADD_TO_BASKET:
-      //check id item is exist 
-      const existingitem = state.baskat.find((item)=>item.id===action.item.id)
-      if(existingitem){
-  return {
-        ...state,
-        basket : [...state.basket,{...action.item,amount:1}]
-      }
-    }else{
-      const updatbasket = state.basket.map((item)=>{
-        
-          item.id ===action.item.id? {...item,amount:item.amount + 1} : item
-        }
-      )
-      return{
-        ...state,
-        basket: updatbasket
+    case type.ADD_TO_BASKET: {
+      // check if item exists
+      const existingItem = state.basket.find((item) => item.id === action.item.id);
 
+      if (!existingItem) {
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedBasket = state.basket.map((item) =>
+          item.id === action.item.id ? { ...item, amount: (item.amount || 0) + 1 } : item
+        );
+
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
       }
     }
 
