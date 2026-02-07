6eb66d7b8cd864f0c15e2616bdeef6408c881add
haymanot <haymanotebabu2@gmail.com>
2026-02-07 11:06:32 +0300

    the express and nessessery pakage to install

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+
+
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+
+
+setGlobalOptions({ maxInstances: 10 });
+
+
