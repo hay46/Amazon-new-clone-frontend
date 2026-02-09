54e7d227f97a0cc86e06fb76cefd1a03132a3269
haymanot <haymanotebabu2@gmail.com>
2026-02-09 23:11:29 +0300

    the cardinfo change based on the baskt add

--- src/Components/card/Cardinfos.jsx
+++ src/Components/card/Cardinfos.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
 import './CardInfos.css'
-import {CardinformationData} from './CardinformationData.js'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useStateValue } from '../dataprovider/Dataprovider.jsx'
-import {Type} from '../../utilitiy/Action.js'
-function CardInfos ({data}) {
-  const [{ basket, user }, dispatch] = useStateValue();
-  const ADD_TO_CART = () => {
-    dispatch({ type: Type.ADD_TO_BASKET, payload: data });
-  };
+import { Type } from '../../utilitiy/Action.js'
 
+function CardInfos({ data }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const ADD_TO_CART = () => {
+    dispatch({
+      type: Type.ADD_TO_BASKET,
+      payload: {
+        id: data.id,
+        title: data.title,
+        image: data.imageLink, // እዚህ ጋር imageLink የሚለውን ወደ image ቀይረነዋል ለReducer እንዲመች
+        price: data.price,
+        rating: data.rating,
+        description: data.description
+      }
+    });
+  };
 
   return (
     <div className="card-information">
       <Link to={`/Card/${data.id}`}>
-        <h1>{data.id}</h1>
-        <span className="card-img">
+        <div className="card-header">
           <h1>{data.title}</h1>
-        </span>
+        </div>
         <img src={data.imageLink} alt={data.title} />
         <p className="img-description">{data.description}</p>
-        <p className="shopping know">ProdactDitail</p>
         <p className="img-price">Price: ${data.price}</p>
       </Link>
-    
-        
-         <button onClick={ADD_TO_CART} className='add-to-cart-props'>
-            Add to Cart
-          </button>
-        
-          
-        
-      
-      
+      <button onClick={ADD_TO_CART} className='add-to-cart-props'>
+        Add to Cart
+      </button>
     </div>
   );
 }
